f2cecbef419012e313aa67f03b7fe2895eda9c01
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create a Funnel chart
+
+
+<p>The following example demonstrates how to create a <a href="http://www.devexpress.com/Help/Content.aspx?help=XtraCharts&document=CustomDocument6222.htm">Funnel</a> chart at runtime.</p><p>Note that this series view type is associated with the <a href="http://devexpress.com/Help/Content.aspx?help=XtraCharts&document=CustomDocument5906.htm">Simple Diagram</a> type, and you should cast your <a href="http://devexpress.com/Help/Content.aspx?help=XtraCharts&document=CustomDocument6017.htm">diagram</a> object to this type, in order to access its specific options.</p>
+
+<br/>
+
+
---
+++ VB/Series_Funnel/Form1.vb
@@ -0,0 +1,59 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+' ...
+
+Namespace Series_Funnel
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			' Create a new chart.
+			Dim funnelChart As New ChartControl()
+
+			' Create a funnel series.
+			Dim series1 As New Series("A Funnel Series", ViewType.Funnel)
+
+			' Add points to the series.
+			series1.Points.Add(New SeriesPoint("A", 48.5))
+			series1.Points.Add(New SeriesPoint("B", 29.6))
+			series1.Points.Add(New SeriesPoint("C", 17.1))
+			series1.Points.Add(New SeriesPoint("D", 13.3))
+			series1.Points.Add(New SeriesPoint("E", 11.6))
+
+			' Add the series to the chart.
+			funnelChart.Series.Add(series1)
+
+			' Adjust the view-type specific properties of the series.
+			Dim myView As FunnelSeriesView = CType(series1.View, FunnelSeriesView)
+
+			myView.Titles.Add(New SeriesTitle())
+			myView.Titles(0).Text = series1.Name
+			myView.HeightToWidthRatioAuto = False
+			myView.HeightToWidthRatio = 1.5
+			myView.PointDistance = 10
+
+			' Adjust the point options of the series.
+			Dim myPointOptions As FunnelPointOptions = CType(series1.PointOptions, FunnelPointOptions)
+
+			myPointOptions.PointView = PointView.ArgumentAndValues
+			myPointOptions.PercentOptions.ValueAsPercent = True
+			myPointOptions.ValueNumericOptions.Format = NumericFormat.Percent
+			myPointOptions.ValueNumericOptions.Precision = 0
+
+			' Specify the series labels position.
+			CType(series1.Label, FunnelSeriesLabel).Position = FunnelSeriesLabelPosition.RightColumn
+
+			' Hide the legend (if necessary).
+			funnelChart.Legend.Visible = False
+
+			' Add the chart to the form.
+			funnelChart.Dock = DockStyle.Fill
+			Me.Controls.Add(funnelChart)
+		End Sub
+	End Class
+End Namespace
